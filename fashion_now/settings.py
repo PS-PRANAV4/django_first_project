ab9a91ace59dd7787be282a70886e55ecd596765
sangeeth ps <pranavpranab@gmail.com>
2022-07-07 20:06:02 +0530

    allowed host

--- fashion_now/settings.py
+++ fashion_now/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ss1#@6dm&&go!akn7xfy4p!@%)qowmjd&%88ll9&k9kbj%yxwt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
