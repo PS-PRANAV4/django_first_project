aa79981949995bc6b82e571c4c52af9ffd420d60
sangeeth ps <pranavpranab@gmail.com>
2022-07-07 21:41:30 +0530

    database changed

--- fashion_now/settings.py
+++ fashion_now/settings.py
@@ -90,16 +90,22 @@ AUTH_USER_MODEL = 'admins.Accounts'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES={
-   'default':{
-      'ENGINE':'django.db.backends.postgresql_psycopg2',
-      'NAME':'fashion_now',
-      'USER':'postgres',
-      'PASSWORD':'pranavpranab#12345',
-      
-   }
+# DATABASES={
+#    'default':{
+#       'ENGINE':'django.db.backends.postgresql_psycopg2',
+#       'NAME':'fashion_now',
+#       'USER':'postgres',
+#       'PASSWORD':'pranavpranab#12345',
+#       'HOST':'localhost',
+#       'PORT':'5432',
+#    }
+# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
