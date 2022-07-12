969fc1241133425b3e0a956040751d31a32e683f
sangeeth ps <pranavpranab@gmail.com>
2022-07-13 00:27:05 +0530

    s

--- fashion_now/settings.py
+++ fashion_now/settings.py
@@ -90,27 +90,27 @@ AUTH_USER_MODEL = 'admins.Accounts'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES={
-   'default':{
-      'ENGINE':'django.db.backends.postgresql_psycopg2',
-      'NAME':'fashion',
-      'USER':'postgres',
-      'PASSWORD':'pranavpranab#12345',
-      'HOST':'database-1.c4ry1lx32rnb.us-west-2.rds.amazonaws.com',
-      'PORT':'5432',
-   }
-} 
+# DATABASES={
+#    'default':{
+#       'ENGINE':'django.db.backends.postgresql_psycopg2',
+#       'NAME':'fashion',
+#       'USER':'postgres',
+#       'PASSWORD':'pranavpranab#12345',
+#       'HOST':'database-1.c4ry1lx32rnb.us-west-2.rds.amazonaws.com',
+#       'PORT':'5432',
+#    }
+# } 
 
 
 
  
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# } 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
